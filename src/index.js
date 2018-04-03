1ec7f2286407c0aa2d0a104ce5daa7481dbc8cc8
Vasabhaktola, Suresh <Suresh.Vasabhaktola@contractor.ca.com>
2018-04-03 15:42:47 -0700

    React starter

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+//we need this for IE11 and Edge
+require('es6-promise').polyfill()
+let Funnel = App
+export default Funnel;
